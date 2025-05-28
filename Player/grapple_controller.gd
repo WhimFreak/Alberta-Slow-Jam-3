9da9b893bb3ed1f6ed0423640f80866a77c4ce96
WhimFreak <whimmyfreaky@gmail.com>
2025-05-28 12:56:31 +0800

    Add files via upload

---
+++ Player/grapple_controller.gd
@@ -0,0 +1,159 @@
+extends Node3D
+
+@export var rest_length: float = 2 # The default length before forces are applied
+@export var stiffness: float = 5 # Higher stiffness pulls the player quicker
+@export var damping: float = 3 # Force lost over time
+
+@export var arm_max_length: float = 100
+@export var arm_extend_speed: float = 200
+
+@onready var player: CharacterBody3D = get_parent()
+@onready var arms: Node3D = %Arms
+
+@onready var left_arm: Node3D = %LeftArm
+@onready var left_arm_cast: RayCast3D = %LeftArmCast
+
+@onready var right_arm: Node3D = %RightArm
+@onready var right_arm_cast: RayCast3D = %RightArmCast
+
+var left_target: Vector3 = Vector3.ZERO
+var left_last_target: Vector3 = Vector3.ZERO
+var right_target: Vector3 = Vector3.ZERO
+var right_last_target: Vector3 = Vector3.ZERO
+
+var left_arm_launched: bool = false
+var left_arm_retracting: bool = false
+var left_arm_connected: bool = false
+
+var right_arm_launched: bool = false
+var right_arm_retracting: bool = false
+var right_arm_connected: bool = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("left_mouse") and !left_arm_launched and !left_arm_retracting:
+		left_arm_launched = true
+	if Input.is_action_just_released("left_mouse"):
+		retract(true)
+		
+	# While attached to a branch, apply force and update arm position/rotation
+	if left_arm_connected and left_target.length() != 0:
+		update_arm(true)
+		update_swing(delta)
+	# Saves the last target position after retracting so it animates properly
+	elif left_last_target.length() != 0:
+		left_arm.look_at(left_last_target)
+		
+	# 'Animating 'throwing' the arm
+	if left_arm_launched and not left_arm_connected:
+		left_arm.scale.z = min(left_arm.scale.z + delta * arm_extend_speed, arm_max_length)
+		
+		# On collision, set target variable and attach to it
+		if left_arm_cast.is_colliding():
+			if left_arm_cast.get_collider().get_collision_layer_value(3) == true: # Branches will have collision layer 3
+				left_target = left_arm_cast.get_collision_point()
+				left_last_target = left_target
+				left_arm_connected = true
+			else: # Retract automatically if it hits a wall
+				retract(true)
+			
+		if left_arm.scale.z >= arm_max_length:
+			retract(true)
+			
+	elif left_arm_retracting:
+		left_arm.scale.z = max(left_arm.scale.z - delta * arm_extend_speed, 0.001) # Animate the arm returning
+		if left_arm.scale.z <= 0.002: # Reset arm's state once returned
+			left_arm_retracting = false
+			left_last_target = Vector3.ZERO
+			left_arm.rotation = Vector3.ZERO
+	
+	# Duplicated for right arm
+	if Input.is_action_just_pressed("right_mouse") and !right_arm_launched and !right_arm_retracting:
+		right_arm_launched = true
+	if Input.is_action_just_released("right_mouse"):
+		retract(false)
+	
+	if right_arm_connected and right_target.length() != 0:
+		update_arm(false)
+		update_swing(delta)
+	elif right_last_target.length() != 0:
+		right_arm.look_at(right_last_target)
+		
+	if right_arm_launched and not right_arm_connected:
+		right_arm.scale.z = min(right_arm.scale.z + delta * arm_extend_speed, arm_max_length)
+		
+		if right_arm_cast.is_colliding():
+			if right_arm_cast.get_collider().get_collision_layer_value(3) == true:
+				right_target = right_arm_cast.get_collision_point()
+				right_last_target = right_target
+				right_arm_connected = true
+			else:
+				retract(false)
+			
+		if right_arm.scale.z >= arm_max_length:
+			retract(false)
+			
+	elif right_arm_retracting:
+		right_arm.scale.z = max(right_arm.scale.z - delta * arm_extend_speed, 0.001)
+		if right_arm.scale.z <= 0.002:
+			right_arm_retracting = false
+			right_last_target = Vector3.ZERO
+			right_arm.rotation = Vector3.ZERO
+	
+func retract(is_left: bool = true):
+	if is_left:
+		left_arm_launched = false
+		left_arm_connected = false
+		left_arm_retracting = true
+	else:
+		right_arm_launched = false
+		right_arm_connected = false
+		right_arm_retracting = true
+		
+	if not player.is_on_floor():
+		player.has_swung = true
+	
+func update_arm(is_left: bool = true):
+	# Attaches the arm to the target position and rotate it around it
+	if is_left:
+		var dist = player.global_position.distance_to(left_target)
+		
+		left_arm.look_at(left_target)
+		left_arm.scale.z = dist
+	else:
+		var dist = player.global_position.distance_to(right_target)
+		
+		right_arm.look_at(right_target)
+		right_arm.scale.z = dist
+	
+func update_swing(delta: float):
+	# Calculate forces from both arms and apply them
+	
+	var left_arm_force := Vector3.ZERO
+	var right_arm_force := Vector3.ZERO
+	
+	if left_arm_connected:
+		left_arm_force = get_spring_force(left_target)
+	if right_arm_connected:
+		right_arm_force = get_spring_force(right_target)
+	
+	player.velocity += (left_arm_force + right_arm_force) * delta
+	
+func get_spring_force(target_pos: Vector3) -> Vector3:
+	# Some math... Idk man, I just stole this from a tutorial
+	var target_direction := player.global_position.direction_to(target_pos)
+	var target_distance := player.global_position.distance_to(target_pos)
+	
+	var displacement := target_distance - rest_length
+	
+	var force := Vector3.ZERO
+	
+	if displacement > 0:
+		var spring_magnitude := stiffness * displacement
+		var spring_force := target_direction * spring_magnitude
+		
+		var velocity_dot = player.velocity.dot(target_direction)
+		var damp = -damping * velocity_dot * target_direction
+		
+		force = spring_force + damp
+	
+	return force
---
+++ Player/player.gd
@@ -0,0 +1,87 @@
+extends CharacterBody3D
+
+@export var mouse_sensitivity: float = 0.25
+@export var walk_speed: float = 15
+@export var acceleration: float = 70
+@export var launched_deceleration: float = 15
+@export var rotation_speed: float = 8
+@export var jump_height: float = 14
+@export var gravity: float = -35
+@export var momentum_drag: float = 0.98
+@export var air_control: float = 30
+
+@onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera_3d: Camera3D = %Camera3D
+@onready var model: Node3D = $Model
+@onready var swing_controller: Node3D = $SwingController
+@onready var arms: Node3D = %Arms
+
+var camera_direction: Vector3 = Vector3.ZERO
+var last_move_direction: Vector3 = Vector3.ZERO
+var has_swung: bool = false
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void: # Camera controls
+	if event.is_action_pressed("left_mouse"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+	if event.is_action_pressed("escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		camera_pivot.rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		camera_pivot.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		camera_pivot.rotation_degrees.x = clamp(camera_pivot.rotation_degrees.x, -80, 90)
+		arms.rotation_degrees = camera_pivot.rotation_degrees
+		
+func _physics_process(delta: float) -> void:
+	camera_direction = Vector3.ZERO
+	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	
+	# Make input directions depend on camera
+	var camera_forward := camera_3d.global_basis.z
+	var camera_right := camera_3d.global_basis.x
+	var move_direction = (camera_forward * input.y + camera_right * input.x)
+	move_direction.y = 0
+	move_direction = move_direction.normalized()
+	
+	if swing_controller.left_arm_connected or swing_controller.right_arm_connected: # Movement while swinging
+		velocity.x += move_direction.x * 30 * delta
+		velocity.z += move_direction.z * 30 * delta
+		if Input.is_action_just_pressed("jump"): # Player can jump while swinging for a slight vertical boost
+			velocity.y += jump_height
+			swing_controller.retract(true)
+			swing_controller.retract(false)
+	elif has_swung:
+		post_swing_movement(move_direction, delta) # For conserving momentum
+	else:
+		normal_movement(move_direction, delta)
+	
+	velocity.y += gravity * delta
+	
+	move_and_slide()
+	
+	if is_on_floor():
+		has_swung = false
+	
+	# Player rotates to face movement direction
+	if move_direction.length() != 0:
+		last_move_direction = move_direction
+	var facing_angle := Vector3.FORWARD.signed_angle_to(last_move_direction, Vector3.UP)	
+	model.global_rotation.y = lerp_angle(model.global_rotation.y, facing_angle, rotation_speed * delta)
+		
+func normal_movement(move_dir: Vector3, delta):
+	velocity.x = move_toward(velocity.x, move_dir.x * walk_speed, acceleration * delta)
+	velocity.z = move_toward(velocity.z, move_dir.z * walk_speed, acceleration * delta)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y += jump_height
+
+func post_swing_movement(move_dir: Vector3, delta):
+	velocity.x += move_dir.x * air_control * delta
+	velocity.z += move_dir.z * air_control * delta
+	
+	velocity.x *= momentum_drag
+	velocity.z *= momentum_drag
